fb76fb2e5494f1cc7bb03110dad974ad2442a571
Kamil Skowronek <kamilskylark@gmail.com>
2025-01-29 15:05:09 +0100

    product views, temlates, urls

--- shop_app/products/urls.py
+++ shop_app/products/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.product_list, name='product_list'),  # Strona z listą produktów
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),  # Szczegóły produktu
+    path('product/add/', views.product_add, name='product_add'),
+    path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),  # Edytowanie produktu
+    path('product/delete/<int:pk>/', views.product_delete, name='product_delete'),
 ]
--- shop_app/products/views.py
+++ shop_app/products/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1>Witaj w naszym sklepie!</h1><p>To strona główna.</p>")
+from .models import Product
+from .forms import ProductForm
+
+def product_list(request):
+    products = Product.objects.all()  # Pobieramy wszystkie produkty
+    return render(request, 'products/product_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)  # Pobieramy produkt na podstawie jego ID
+    return render(request, 'products/product_detail.html', {'product': product})
+
+
+def product_add(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')  # Przekierowanie na stronę listy produktów
+    else:
+        form = ProductForm()
+    return render(request, 'products/product_add.html', {'form': form})
+
+
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_detail', pk=product.pk)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'products/product_edit.html', {'form': form, 'product': product})
+
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product.delete()
+    return redirect('product_list')  # Przekierowanie na stronę listy produktów
