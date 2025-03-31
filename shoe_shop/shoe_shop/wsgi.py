38d922ec97d227efa1ccb97207e86b499475c5e4
Kamil Skowronek <kamilskylark@gmail.com>
2025-04-01 01:31:05 +0200

    Refactor WSGI configuration to improve clarity and add Vercel compatibility

--- shoe_shop/shoe_shop/wsgi.py
+++ shoe_shop/shoe_shop/wsgi.py
@@ -1,18 +1,15 @@
-"""
-WSGI config for Weiboo project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-import os, sys
-
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
+# Dodanie ścieżki nadrzędnej do sys.path, aby znaleźć settings.py
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoe_shop.settings')
+# Ustawienie Django settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shoe_shop.settings")
 
+# Tworzenie aplikacji WSGI
 application = get_wsgi_application()
+
+# Wymagane przez Vercel
+app = application  # Vercel wymaga zmiennej 'app'
