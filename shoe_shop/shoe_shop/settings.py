e7c62ed55321daf3b3ef42b9439b32cd6812d8ae
Kamil Skowronek <kamilskylark@gmail.com>
2025-04-01 02:09:02 +0200

    Refactor static file settings for improved path handling and clarity

--- shoe_shop/shoe_shop/settings.py
+++ shoe_shop/shoe_shop/settings.py
@@ -14,17 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import os
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "shoe_shop/static")
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 
 LOGIN_URL = '/users/login/'
 
@@ -136,6 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
